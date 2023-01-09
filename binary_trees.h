7fac41bcd7c6e64bfbc0b61a4561c5453abc397c
EXTRADEEN <dinumira@gmail.com>
2023-01-09 05:49:22 -0800

    added 0-binary_tree_node - function that create a binary tree node and updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,9 @@
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,7 +21,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
